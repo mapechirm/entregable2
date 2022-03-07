efd71030a97ee8ff1cf996ea27a5618bfb4df3a1
Antonio Pechir <mapechirm@hotmail.com>
2022-03-07 01:34:59 -0600

    Bootstraaaaaaaaaaaaaaaaaaaaaaaaaaap

--- resources/views/components/template.blade.php
+++ resources/views/components/template.blade.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+    {{-- <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}"> --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     @if ($style)
     <style>
       {{ $stylecontent }}    
@@ -28,7 +29,8 @@
         {{ $content }}
     </div>
     
-    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+    {{-- <script src="{{ asset('js/bootstrap.min.js') }}"></script> --}}
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="/js/platformAdd.js"></script>
     <script src="https://kit.fontawesome.com/ee80b1599b.js" crossorigin="anonymous"></script>
   </body>
